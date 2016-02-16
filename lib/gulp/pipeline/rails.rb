d8bbb6b2c65097ede4a241a8072b97306194dde4
Kevin Ross <kevin.ross@alienfast.com>
2016-02-16 10:26:07 -0600

    comment usage in case code is inspected instead of readme

--- lib/gulp/pipeline/rails.rb
+++ lib/gulp/pipeline/rails.rb
@@ -3,7 +3,9 @@ require 'gulp/pipeline/rails/version'
 module Gulp
   module Pipeline
     module Rails
-      # Your code goes here...
+      # To use, remove sprockets from your application.rb and initialize Rails with gulp-pipeline-rails
+      #       #require 'rails/all' <== DELETE this line
+      #       require 'gulp/pipeline/rails/all' # <== ADD this line
     end
   end
 end
