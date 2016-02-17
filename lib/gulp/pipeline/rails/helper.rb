4ed1aac4e3d9217531447b2b332f93ec5b9bc85b
Kevin Ross <kevin.ross@alienfast.com>
2016-02-17 16:16:32 -0600

    remove unused map

--- lib/gulp/pipeline/rails/helper.rb
+++ lib/gulp/pipeline/rails/helper.rb
@@ -2,16 +2,6 @@ module Gulp
   module Pipeline
     module Rails
       module Helper
-        # Maps asset types to public directory.
-        ASSET_PUBLIC_DIRECTORIES = {
-          audio:      '/audios',
-          font:       '/fonts',
-          image:      '/images',
-          javascript: '/javascripts',
-          stylesheet: '/stylesheets',
-          video:      '/videos'
-        }
-
         # Computes asset path to public directory. Plugins and
         # extensions can override this method to point to custom assets
         # or generate digested paths or query strings.
