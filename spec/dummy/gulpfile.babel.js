e2fabe1b82e977b01e5de4835e19f38e34221f24
Kevin Ross <kevin.ross@alienfast.com>
2016-03-21 12:15:44 -0500

    updating to gulp-pipeline 4.x

--- spec/dummy/gulpfile.babel.js
+++ spec/dummy/gulpfile.babel.js
@@ -1,58 +1,56 @@
-import {Preset, Clean, CleanDigest, Images, MinifyCss, Sass, RollupIife, ScssLint, EsLint, Rev, TaskSeries} from 'gulp-pipeline'
-
-// debug the project source - remove for repo
-//import {Clean, CleanDigest, Images, MinifyCss, Sass, RollupIife, ScssLint, EsLint, Rev, TaskSeries} from 'gulp-pipeline'
-//import Preset from '../../../gulp-pipeline/src/preset'
+import {Preset, Clean, CleanDigest, CssNano, Images, Sass, RollupIife, ScssLint, EsLint, Rev, Aggregate, parallel, series} from 'gulp-pipeline'
 
 import stringify from 'stringify-object'
 import gulp from 'gulp'
 import findup from 'findup-sync'
 const node_modules = findup('node_modules')
 
-
 let preset = Preset.rails()
 
-let eslint = new EsLint(gulp, preset)
-let scsslint = new ScssLint(gulp, preset)
-
 // When converting non-modular dependencies into usable ones using rollup-plugin-commonjs, if they don't have properly read exports add them here.
-let namedExports = {}
+const namedExports = {}
 //namedExports[`${node_modules}/corejs-typeahead/dist/bloodhound.js`] = ['Bloodhound']
 
-let rollup = new RollupIife(gulp, preset, {
-  options: {
-    dest: 'application.js'
-  },
+const rollupConfig = {
   commonjs: {
     options: {
-      namedExports: namedExports
+      namedExports: namedExports,
     }
   }
-})
+}
 
-let lint = [scsslint, eslint]
+const js = new Aggregate(gulp, 'js',
+  series(gulp,
+    new EsLint(gulp, preset),
+    new RollupIife(gulp, preset, {options: {dest: 'application.js'}}, rollupConfig)
+  )
+)
 
-// instantiate ordered array of recipes (for each instantiation the tasks will be created e.g. sass and sass:watch)
-let recipes = [
-  new Clean(gulp, preset),
-  lint,
-  [
-    new Images(gulp, preset),
-    new Sass(gulp, preset),
-    rollup
-  ]
-]
+const css = new Aggregate(gulp, 'css',
+  series(gulp,
+    new ScssLint(gulp, preset),
+    new Sass(gulp, preset)
+  )
+)
 
-// Simple helper to create the default and watch tasks as a sequence of the recipes already defined
-new TaskSeries(gulp, 'default', recipes)
-new TaskSeries(gulp, 'lint', lint)
-new TaskSeries(gulp, 'js', [eslint, rollup])
-
+new Aggregate(gulp, 'default',
+  series(gulp,
+    new Clean(gulp, preset),
+    parallel(gulp,
+      new Images(gulp, preset),
+      js,
+      css
+    )
+  )
+)
 
 // Create the production assets
-let digest = [
-  new CleanDigest(gulp, preset),
-  new Rev(gulp, preset),
-  new MinifyCss(gulp, preset)
-]
-new TaskSeries(gulp, 'digest', digest)
+new Aggregate(gulp, 'digest',
+  series(gulp,
+    new CleanDigest(gulp, preset),
+    parallel(gulp,
+      new Rev(gulp, preset),
+      new CssNano(gulp, preset)
+    )
+  )
+)
